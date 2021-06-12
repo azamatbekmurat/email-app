50ce70a142e7799316d3da1c8b78d54be3583040
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2021-06-12 00:34:26 -0700

    add react-router, material-ui dependecies, and modify App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./App.css";
+import EmailList from "./components/emailList/EmailList";
+import Header from "./components/header/Header";
+import Mail from "./components/mail/Mail";
+import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Lets build gmail project</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
