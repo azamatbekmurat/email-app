da077017ac0439e7d7ebe8632915da45e94c1e43
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2021-06-12 00:48:53 -0700

    modify EmailList component

--- src/components/emailList/EmailList.jsx
+++ src/components/emailList/EmailList.jsx
@@ -5,6 +5,11 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+
 function EmailList() {
     return (
         <div className="emailList">
@@ -22,9 +27,22 @@ function EmailList() {
                     </IconButton>
                 </div>
                 <div className="emailList__settingsRight">
-                    
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
                 </div>
             </div>
+
+            <div className="emailList__sections"></div>
             
         </div>
     )
