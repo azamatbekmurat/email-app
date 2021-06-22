11c30934f0687004fbf62a4f547268a58f46dec7
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2021-06-21 19:28:01 -0700

    add reducer for the state of user login and logout

--- src/App.js
+++ src/App.js
@@ -8,30 +8,36 @@ import SendMail from "./components/sendMail/SendMail";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
+import { selectUser } from "./features/userSlice";
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
 
   return (
     <Router>
-      <div className="app">
-        <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
 
-        <div className="app__body">
-          <Sidebar />
+          <div className="app__body">
+            <Sidebar />
 
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+
+          {sendMessageIsOpen && <SendMail />}
         </div>
-
-        {sendMessageIsOpen && <SendMail />}
-      </div>
+      )}
     </Router>
   );
 }
